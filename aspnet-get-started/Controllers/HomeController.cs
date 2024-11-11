2a85ef96299cc1bad625b554e36cdc218255dad2
johnbilliris <johnbilliris@users.noreply.github.com>
2024-11-11 15:52:12 +1100

    Added functionality to delay response times for better constant and random load testing

--- aspnet-get-started/Controllers/HomeController.cs
+++ aspnet-get-started/Controllers/HomeController.cs
@@ -1,13 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Diagnostics;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
 namespace aspnet_get_started.Controllers
 {
+    public enum ResponseMode
+    {
+        Constant = 0,
+        Variable = 1
+    }
+
     public class HomeController : Controller
     {
+        private readonly ResponseMode responseMode = ResponseMode.Constant;
+        private long responseDelay = 300;
+
+        private Stopwatch stopwatch = new Stopwatch();
+        private long actualDelay = 0;
+
+        public HomeController()
+        {
+            try
+            {
+                responseMode = (ResponseMode)Enum.Parse(typeof(ResponseMode), ConfigurationManager.AppSettings["ResponseMode"]);
+                responseDelay = long.Parse(ConfigurationManager.AppSettings["ResponseDelay"]);
+            }
+            catch (Exception)
+            {
+            }
+            CauseResponseDelay();
+        }
+
         public ActionResult Index()
         {
             return View();
@@ -25,6 +52,34 @@ namespace aspnet_get_started.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        private void CauseResponseDelay()
+        {
+            stopwatch.Start();
+            if (responseMode == ResponseMode.Constant)
+            {
+                while (stopwatch.ElapsedMilliseconds < responseDelay)
+                {
+                    int dummyInteger = 1;
+                    dummyInteger += 1;
+                }
+            }
+            else
+            {
+                Random random = new Random();
+                responseDelay = random.Next(100, 500);
+                while (stopwatch.ElapsedMilliseconds < responseDelay)
+                {
+                    // Do nothing
+                }
+            }
+            stopwatch.Stop();
+            actualDelay = stopwatch.ElapsedMilliseconds;
+
+            ViewBag.ResponseMode = responseMode.ToString();
+            ViewBag.ResponseDelay = responseDelay;
+            ViewBag.ActualDelay = actualDelay;
         }
     }
 }
